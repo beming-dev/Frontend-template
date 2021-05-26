05edca1325a662fc5f5cc75003a66c252508f3b8
beming-dev <mingfordev@gmail.com>
2021-05-26 11:02:52 +0900

    Update slider function

--- music/slider.js
+++ music/slider.js
@@ -1,4 +1,3 @@
-let slider = document.querySelector('slider');
 let items = document.getElementsByClassName('slider-item');
 
 [...items].map((item) => {
@@ -6,10 +5,39 @@ let items = document.getElementsByClassName('slider-item');
 })
 
 function onItemClick(e){
-    console.log(e.target.dataset.count);
+    let center = document.querySelector(".center");
+    let slider = document.querySelector('.slider');
+
     [...items].map((item) =>{
-        item.style = 'transform: translate3d(150px, 0, 0)';
-        item.classList.remove("center");
+        if(item.classList.contains("center")){
+            item.classList.remove("center");
+            item.classList.add("popular");
+        }
     })
+    e.target.classList.remove("popular");
     e.target.classList.add("center");
+
+    let diff = center.dataset.count - e.target.dataset.count;
+    switch(diff){
+        case -2:
+            // moveItem(slider.clientWidth * -17 /100);
+            slider.appendChild(slider.firstElementChild);
+            slider.appendChild(slider.firstElementChild);
+            break;
+        case -1:
+            // slider.appendChild(slider.firstElementChild);
+            slider.appendChild(slider.firstElementChild);
+            break;
+        case 1:
+            slider.insertBefore(slider.lastElementChild, container.firstElementChild);
+            break;
+        case 2:
+            slider.insertBefore(slider.lastElementChild, slider.firstElementChild);
+            slider.insertBefore(slider.lastElementChild, slider.firstElementChild);
+            break;
+    }
+
+    [...items].map((item, index) => {
+        item.dataset.count = index + 1;
+    })
 }

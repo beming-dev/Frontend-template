95f179bc0dc6003599916bc0f86aaacbdf66a7fb
beming-dev <mingfordev@gmail.com>
2021-05-26 11:43:46 +0900

    Update slider function

--- music/slider.js
+++ music/slider.js
@@ -18,26 +18,28 @@ function onItemClick(e){
     e.target.classList.add("center");
 
     let diff = center.dataset.count - e.target.dataset.count;
-    switch(diff){
-        case -2:
-            // moveItem(slider.clientWidth * -17 /100);
-            slider.appendChild(slider.firstElementChild);
-            slider.appendChild(slider.firstElementChild);
-            break;
-        case -1:
-            // slider.appendChild(slider.firstElementChild);
-            slider.appendChild(slider.firstElementChild);
-            break;
-        case 1:
-            slider.insertBefore(slider.lastElementChild, container.firstElementChild);
-            break;
-        case 2:
-            slider.insertBefore(slider.lastElementChild, slider.firstElementChild);
-            slider.insertBefore(slider.lastElementChild, slider.firstElementChild);
-            break;
-    }
+    slider.style.transform = `translateX(${slider.clientWidth * 17 /100 * diff}px)`;
 
-    [...items].map((item, index) => {
-        item.dataset.count = index + 1;
-    })
+    setTimeout (() => {
+        slider.removeAttribute('style');
+        switch(diff){
+            case -2:
+                slider.appendChild(slider.firstElementChild);
+                slider.appendChild(slider.firstElementChild);
+                break;
+            case -1:
+                slider.appendChild(slider.firstElementChild);
+                break;
+            case 1:
+                slider.insertBefore(slider.lastElementChild, slider.firstElementChild);
+                break;
+            case 2:
+                slider.insertBefore(slider.lastElementChild, slider.firstElementChild);
+                slider.insertBefore(slider.lastElementChild, slider.firstElementChild);
+                break;
+        }
+        [...items].map((item, index) => {
+            item.dataset.count = index + 1;
+        })
+    }, 5000);
 }

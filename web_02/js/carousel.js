577f3cac39a93a48a0eeacb71eb63d4dd5b58708
beming-dev <mingfordev@gmail.com>
2021-06-15 17:42:02 +0900

    Create login page

--- web_02/js/carousel.js
+++ web_02/js/carousel.js
@@ -16,19 +16,16 @@ class Carousel {
       el.classList.remove('gallery-item-1');
       el.classList.remove('gallery-item-2');
       el.classList.remove('gallery-item-3');
-      el.classList.remove('gallery-item-4');
-      el.classList.remove('gallery-item-5');
     });
 
-    this.carouselArray.slice(0, 5).forEach((el, i) => {
+    this.carouselArray.slice(0, 3).forEach((el, i) => {
       el.classList.add(`gallery-item-${i+1}`);
     });
   }
 
   // Update the current order of the carouselArray and gallery
   setCurrentState(direction) {
-
-    if (direction.className == 'gallery-controls-previous') {
+    if (direction.className == 'gallery-controls-item gallery-controls-previous') {
       this.carouselArray.unshift(this.carouselArray.pop());
     } else {
       this.carouselArray.push(this.carouselArray.shift());
@@ -65,28 +62,7 @@ class Carousel {
     triggers.forEach(control => {
       control.addEventListener('click', e => {
         e.preventDefault();
-
-        if (control.className == 'gallery-controls-add') {
-          const newItem = document.createElement('img');
-          const latestItem = this.carouselArray.length;
-          const latestIndex = this.carouselArray.findIndex(item => item.getAttribute('data-index') == this.carouselArray.length)+1;
-
-          // Assign the necessary properties for new gallery item
-          Object.assign(newItem,{
-            className: 'gallery-item',
-            src: `assets/images/albums/carousel${this.carouselArray.length+1}`
-          });
-          newItem.setAttribute('data-index', this.carouselArray.length+1);
-
-          // Then add it to the carouselArray and update the gallery
-          this.carouselArray.splice(latestIndex, 0, newItem);
-          document.querySelector(`[data-index="${latestItem}"]`).after(newItem);
-          this.updateGallery();
-
-        } else {
           this.setCurrentState(control);
-        }
-
       });
     });
   }

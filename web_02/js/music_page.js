7d411368bc4de12e96ccf5b4b26339834cac3c6a
beming-dev <mingfordev@gmail.com>
2021-06-24 10:11:19 +0900

    Update responsive

--- web_02/js/music_page.js
+++ web_02/js/music_page.js
@@ -21,13 +21,21 @@ let heart_click = [false, false, false, false, false];
 [...albumBox].map((item, index) => {
     item.addEventListener('mouseover', () => {
         let image = document.querySelector(`.album-box0${index+1} .album-image`);
+        let txt1 = document.querySelector(`.album-box0${index+1} .album-name`);
+        let txt2 = document.querySelector(`.album-box0${index+1} .album-singer`);
         image.style.opacity = "0.4";
+        txt1.style.color = "black";
+        txt2.style.color = "black";
     })
 });
 
 [...albumBox].map((item, index) => {
     item.addEventListener('mouseout', () => {
         let image = document.querySelector(`.album-box0${index+1} .album-image`);
+        let txt1 = document.querySelector(`.album-box0${index+1} .album-name`);
+        let txt2 = document.querySelector(`.album-box0${index+1} .album-singer`);
         image.style.opacity = "1";
+        txt1.style.color = "white";
+        txt2.style.color = "#c1c1c1";
     })
 });
